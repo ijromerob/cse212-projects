576c0f7c52e7e49964af0ff9b3ab55525607fd0f
ijromerob <ijromerob@gmail.com>
2024-11-13 19:17:52 -0700

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,68 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following items: A(2),B(5),C(3) and run until it is empty
+    // Expected Result: B,C,A
+    // Defect(s) Found: There was not count of the lists that would aid the process of ending the loop, the for loop was starting at 1 and needed to start at 0, it wasn't removing the item with highest priority - PASSED
     public void TestPriorityQueue_1()
     {
+
+        var A = new PriorityItem("A",2);
+        var B = new PriorityItem("B",5);
+        var C = new PriorityItem("C",3);
+
+        PriorityItem[] expectedResult = [B,C,A];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue(A.Value,A.Priority);
+        priorityQueue.Enqueue(B.Value,B.Priority);
+        priorityQueue.Enqueue(C.Value,C.Priority);
+
+        int i = 0;
+        while (0 < priorityQueue.Count)
+        {
+          if (expectedResult.Length <= i)
+          {
+            Assert.Fail("Queue should have finished");
+          }
+          var item = priorityQueue.Dequeue();
+          Assert.AreEqual(expectedResult[i].Value, item);
+          i++;
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following items: A(2),B(5),C(5), D(3) and run until it is empty
+    // Expected Result:B,C,D,A
+    // Defect(s) Found: the index assignment was not working properly since the condition was changing the index whenever there was another element with a highest priority - PASSED
     public void TestPriorityQueue_2()
     {
+        var A = new PriorityItem("A",2);
+        var B = new PriorityItem("B",5);
+        var C = new PriorityItem("C",5);
+        var D = new PriorityItem("D",3);
+
+        PriorityItem[] expectedResult = [B,C,D,A];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue(A.Value,A.Priority);
+        priorityQueue.Enqueue(B.Value,B.Priority);
+        priorityQueue.Enqueue(C.Value,C.Priority);
+        priorityQueue.Enqueue(D.Value,D.Priority);
+
+        int i = 0;
+        while (0 < priorityQueue.Count)
+        {
+          if (expectedResult.Length <= i)
+          {
+            Assert.Fail("Queue should have finished");
+          }
+          var item = priorityQueue.Dequeue();
+          Assert.AreEqual(expectedResult[i].Value, item);
+          i++;
+        }
     }
 
     // Add more test cases as needed below.
